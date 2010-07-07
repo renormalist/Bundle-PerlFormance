7c0edf15aad04e50e49edf741f82deb7de50507e
Steffen Schwigon <ss5@renormalist.net>
2010-07-07 23:46:16 +0200

    +MooseX::Declare

--- lib/Bundle/PerlFormance.pm
+++ lib/Bundle/PerlFormance.pm
@@ -1,6 +1,6 @@
 package Bundle::PerlFormance;
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 1;
 
@@ -51,6 +51,8 @@ NetAddr::IP
 HTML::Parser
 
 Mail::SpamAssassin
+
+MooseX::Declare
 
 Moose
 
